b87a14a5fc28ce802ad3e477193276af8b27d3f3
catanielena <catani.elena1@gmail.com>
2021-08-26 09:33:27 +0200

    Add: style

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,16 +1,35 @@
 // Lista mail
-var mailList = ["martamodonesi@gmail.com", "biagiomoro85@gmail.com", "gaiavittoria.milanese@gmail.com", "lofarosalvatoreemanuele@gmail.com"];
-// Richiesta mail
-var mailRequest = prompt("Inserisci la tua mail", "@gmail.com");
-// Verifica che la mail inserita sia nella lista
-var login = 0;
-for (i = 0; i < mailList.length; i++) {
-    if (mailRequest == mailList[i]) {
-        login = 1;
-        alert("Accedi");
-    } 
-}
+// var mailList = ["martamodonesi@gmail.com", "biagiomoro85@gmail.com", "gaiavittoria.milanese@gmail.com", "lofarosalvatoreemanuele@gmail.com"];
+// // Richiesta mail
+// // var mailRequest = prompt("Inserisci la tua mail", "@gmail.com");
 
-if (login == 0) {
-    alert("Register");
+// var mailRequest = document.getElementById("mail").value;
+
+// // Verifica che la mail inserita sia nella lista
+// var submit = 0;
+// for (i = 0; i < mailList.length; i++) {
+//     if (mailRequest == mailList[i]) {
+//         login = 1;
+//         alert("Accedi");
+//     } 
+// }
+
+// if (login == 0) {
+//     alert("Register");
+// }
+
+function myFunction() {
+    var mailList = ["martamodonesi@gmail.com", "biagiomoro85@gmail.com", "gaiavittoria.milanese@gmail.com", "lofarosalvatoreemanuele@gmail.com"];
+    var mailRequest = document.getElementById("mail").value;
+    var submit = 0;
+    for (i = 0; i < mailList.length; i++) {
+        if (mailRequest == mailList[i]) {
+            submit = 1;
+            alert("Accedi");
+        } 
+    }
+    
+    if (submit == 0) {
+        alert("Register");
+    }
 }
