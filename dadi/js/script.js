8c63032bcf5d7d704819faf43aca736934c1f41a
catanielena <catani.elena1@gmail.com>
2021-08-26 09:50:46 +0200

    Fix: dadi_js

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,10 +1,12 @@
 // Generare un numero random da 1 a 6 per il giocatore
-var playerNumb = alert("Il tuo numero è " + (Math.floor(Math.random() * 100) +1));
+var playerNumb = alert("Il tuo numero è " + (Math.floor(Math.random() * 6) +1));
 // Generare un numero random da 1 a 6 per il computer
-var pcNumb = alert("Il numero del computer è " + (Math.floor(Math.random() * 100) +1));
+var cpuNumb = alert("Il numero della cpu è " + (Math.floor(Math.random() * 6) +1));
 // Se il numero del giocatore è più alto del numero del computer inviare il messaggio "Hai vinto", altrimento "Hai perso"
-if (playerNumb > pcNumb) {
+if (playerNumb > cpuNumb) {
     alert("Hai vinto!");
-} else {
+} else if (cpuNumb > playerNumb) {
     alert("Hai perso! Ritenta");
+} else {
+    alert ("Pareggio!")
 }
